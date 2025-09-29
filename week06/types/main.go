e37db8a9a09459d61119c16ee96bda75bf2fc90e
jks <jksjks8357@gmail.com>
2025-09-29 13:56:49 +0900

    v0.1 type/main.go

---
+++ week06/types/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.91))
+	fmt.Println(math.Ceil(2.91))
+	fmt.Println(math.Round(2.91))
+	fmt.Println(math.Round(2.3))
+	fmt.Println(strings.Title("this is head"))
+}
