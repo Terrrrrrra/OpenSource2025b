95a7031a0e83259398a7f92ad321a695ad6343c6
jks <jksjks8357@gmail.com>
2025-09-29 14:14:44 +0900

    v0.2 type/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,10 +6,15 @@ import (
 	"strings"
 )
 
+// Ctrl+F5
 func main() {
 	fmt.Println(math.Floor(2.91))
 	fmt.Println(math.Ceil(2.91))
 	fmt.Println(math.Round(2.91))
 	fmt.Println(math.Round(2.3))
 	fmt.Println(strings.Title("this is head"))
+
+	fmt.Println("Kim\nInha\t\"\\") // C like
+
+	fmt.Println('A', '가')
 }
