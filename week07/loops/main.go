aed56f74c2ed9d9f369d0b0822513783f71aca1d
jks <jksjks8357@gmail.com>
2025-10-13 14:22:16 +0900

    time and strings.NewReplacer

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -3,12 +3,41 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strings"
+	"time"
 )
 
 func main() {
+	Casting()
+	fmt.Println("\n------------------\n")
+	TimeEX()
+	fmt.Println("\n------------------\n")
+	Swap()
+}
+
+func Casting() {
 	var length float64 = 1.2
 	var width int = 2
 	fmt.Println("면적은", int(length)*width)
 	fmt.Println("length > width", int(length) > width)
 	fmt.Println("원본", reflect.TypeOf(length))
+}
+
+func TimeEX() {
+	var now time.Time = time.Now()
+	var month time.Month = now.Month() //month := now.Month()
+	var day int = now.Day()
+	fmt.Println(month, day)
+}
+
+func Swap() {
+	broken := "G# r#ck!"
+	replacer := strings.NewReplacer("#", "o")
+	fixed := replacer.Replace(broken)
+	fmt.Println(fixed)
+
+	univ := "Go$ inha$"
+	changer := strings.NewReplacer("$", "!")
+	changed := changer.Replace(univ)
+	fmt.Println(changed)
 }
