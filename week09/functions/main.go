fbe2141d9274c9d407952b9b5e098e5b526ac764
jks <jksjks8357@gmail.com>
2025-10-27 15:00:09 +0900

    err make | pointer

---
+++ week09/functions/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
+	area := width * height
+	return area / 10.0, nil
+}
+
+func swap(first *int, second *int) {
+	temp := *first
+	*first = *second
+	*second = temp
+	fmt.Println(*first, *second)
+}
+
+func main() {
+	amount, err := paintNeeded(5.2, 3.5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%0.2f liters needed\n", amount)
+	// amount, err = paintNeeded(4.2, -3.0)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("%0.2f liters needed\n", amount)
+
+	a, b := 10, 20
+	fmt.Println(a, b)
+	swap(&a, &b)
+	fmt.Println(a, b)
+}
