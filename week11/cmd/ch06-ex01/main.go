cebda4b646e35797b1ceeed054aa3b070db11268
jks <jksjks8357@gmail.com>
2025-11-10 14:33:31 +0900

    week11 slice v0.3

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -10,9 +10,16 @@ func main() {
 	// subjects[0] = "Go"
 	// subjects[2] = "Python"
 
-	subjects := []string{"Go", "", "Python"} // slice literal
+	subjects := []string{"Go", "Javascript", "Python", "Linux"} // slice literal
+	subjectsSlice := subjects[1:3]                              // slicing
 
 	for _, subject := range subjects {
 		fmt.Println(subject)
+	}
+
+	fmt.Println("===================")
+
+	for i := 0; i < len(subjectsSlice); i++ {
+		fmt.Println(subjectsSlice[i])
 	}
 }
