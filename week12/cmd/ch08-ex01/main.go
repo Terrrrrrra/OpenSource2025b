55c63ad2da496d21820f051d7f48566111143ddb
jks <jksjks8357@gmail.com>
2025-11-17 15:45:38 +0900

    구조체 0.2

--- week12/cmd/ch08-ex01/main.go
+++ week12/cmd/ch08-ex01/main.go
@@ -8,13 +8,19 @@ type subcriber struct {
 }
 
 func applyPrice(s *subcriber) {
-	s.price = 10000
-	s.name = "Pack Inha"
+	(*s).price = 10000
+	(*s).name = "Pack Inha"
+	// 이렇게 해도 됨
+	// s.price = 10000
+	// s.name = "Pack Inha"
 }
 
 func main() {
 	var s1 subcriber
+	var p *subcriber = &s1
 	s1.name = "Kim Inha"
 	applyPrice(&s1)
 	fmt.Println(s1)
+	fmt.Println((*p).price)
+	fmt.Println(p.price)
 }
